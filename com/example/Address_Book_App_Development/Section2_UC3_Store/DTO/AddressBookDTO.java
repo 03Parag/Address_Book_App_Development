ebf848bb8dd5d37c5d1065b37609a303c5414224
Parag Agarwal <0111al211117.parag@gmail.com>
2025-03-05 15:09:14 +0530

    [Parag Agrawal] Added problem for the Services Layer ability to store the AddressBook Data

---
+++ com/example/Address_Book_App_Development/Section2_UC3_Store/DTO/AddressBookDTO.java
@@ -0,0 +1,33 @@
+package com.example.Address_Book_App_Development.Section2_UC3_Store.DTO;
+
+public class AddressBookDTO {
+
+    private String name;
+    private String phoneNumber;
+    private String email;
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
