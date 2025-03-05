fb38def28550c842e0b7d5d123fa1cc27cdb7e77
Parag Agarwal <0111al211117.parag@gmail.com>
2025-03-05 11:39:24 +0530

    [Parag Agrawal] Added problem to create a address book spring project to cater to REST request from address book UI

---
+++ com/example/Address_Book_App_Development/Section1_UC1_UI/Model/AddressBookModel.java
@@ -0,0 +1,42 @@
+package com.example.Address_Book_App_Development.Section1_UC1_UI.Model;
+
+public class AddressBookModel {
+
+    private String name;
+    private String phoneNumber;
+    private String email;
+    private String address;
+
+    // Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
