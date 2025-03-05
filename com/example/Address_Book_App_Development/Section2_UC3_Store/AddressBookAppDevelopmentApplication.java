fa9bda6b8ab6dbea92e1db7faba53a85f8848c3b
Parag Agarwal <0111al211117.parag@gmail.com>
2025-03-05 15:06:10 +0530

    [Parag Agrawal] Added problem for the Services Layer ability to store the AddressBook Data

---
+++ com/example/Address_Book_App_Development/Section2_UC3_Store/AddressBookAppDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.Address_Book_App_Development.Section2_UC3_Store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookAppDevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressBookAppDevelopmentApplication.class, args);
+	}
+
+}
