cb56a35fa945e021ba148dcf272ea6bfd7bec72e
Parag Agarwal <0111al211117.parag@gmail.com>
2025-03-05 11:37:15 +0530

    [Parag Agrawal] Added problem to create a address book spring project to cater to REST request from address book UI

---
+++ com/example/Address_Book_App_Development/Section1_UC1_UI/AddressBookAppDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.Address_Book_App_Development.Section1_UC1_UI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookAppDevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressBookAppDevelopmentApplication.class, args);
+	}
+
+}
