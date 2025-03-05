ab95c203a8a41ef11736b688ea1a84152376abe4
Parag Agarwal <0111al211117.parag@gmail.com>
2025-03-05 11:42:45 +0530

    [Parag Agrawal] Added problem to create a Rest Controller to demonstrate the various HTTP Methods

---
+++ com/example/Address_Book_App_Development/Section1_UC2_HTTP_Methods/AddressBookAppDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.Address_Book_App_Development.Section1_UC2_HTTP_Methods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookAppDevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressBookAppDevelopmentApplication.class, args);
+	}
+
+}
