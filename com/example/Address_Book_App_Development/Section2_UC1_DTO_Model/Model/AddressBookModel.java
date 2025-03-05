54aaad06e928c7197b44f3f7dc129b19ee47141d
Parag Agarwal <0111al211117.parag@gmail.com>
2025-03-05 11:53:13 +0530

    [Parag Agrawal] Added problem to introduce DTO and Model to AddressBook App

---
+++ com/example/Address_Book_App_Development/Section2_UC1_DTO_Model/Model/AddressBookModel.java
@@ -0,0 +1,50 @@
+package com.example.Address_Book_App_Development.Section2_UC1_DTO_Model.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class AddressBookModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String phoneNumber;
+    private String email;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
