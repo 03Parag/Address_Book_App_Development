ac6c9d8c414efe91549c943f1f7343fe3ace3829
Parag Agarwal <0111al211117.parag@gmail.com>
2025-03-05 11:45:25 +0530

    [Parag Agrawal] Added problem to create a Rest Controller to demonstrate the various HTTP Methods

---
+++ com/example/Address_Book_App_Development/Section1_UC2_HTTP_Methods/Model/AddressBookModel.java
@@ -0,0 +1,41 @@
+package com.example.Address_Book_App_Development.Section1_UC2_HTTP_Methods.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class AddressBookModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String name;
+    private String email;
+    private String phoneNumber;
+
+    // Getters and Setters
+       public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
